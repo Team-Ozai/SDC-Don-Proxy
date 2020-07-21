2c3f6e1c9026ddc0884f70611891fa9f91bd288f
area363 <kidonseo@gmail.com>
2020-07-21 02:25:55 -0700

    Added Docker Configuration

--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -1,23 +1,26 @@
 var newrelic = require('newrelic');
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express();
 
-const rewardsServiceRoute =
-  'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
+const app = express();
+app.use(morgan('tiny'));
+
+// const rewardsServiceRoute =
+//   'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
 
 const proxyRouter = {
-  '/api/banner': 'http://localhost:3002',
-  'api/video': 'http://localhost:3002',
-  'api/update': 'http://localhost:3001',
-  'api/comment': 'http://localhost:3001',
-  'api/story': 'http://localhost:3003',
-  'api/RisksAndChallenges': 'http://localhost:3003',
-  'api/EnvironmentalCommitments': 'http://localhost:3003',
-  'api/projects': rewardsServiceRoute,
-  'api/rewards': rewardsServiceRoute,
+  // '/api/banner': 'http://localhost:3002',
+  // '/api/video': 'http://localhost:3002',
+  // '/api/update': 'http://localhost:3001',
+  // '/api/comment': 'http://localhost:3001',
+  '/api/story': 'http://13.209.179.203:3003',
+  '/api/risksandchallenges': 'http://13.209.179.203:3003',
+  '/api/environmentalcommitments': 'http://13.209.179.203:3003',
+  // '/api/projects': rewardsServiceRoute,
+  // '/api/rewards': rewardsServiceRoute,
 };
 
 app.use(express.static(path.join(__dirname, '../dist')));
